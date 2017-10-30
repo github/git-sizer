1e530cb7cf8ddebad6f3a8491f25df7de3de2ba5
Michael Haggerty <mhagger@github.com>
2017-10-30 18:15:08 +0100

    Change `ToDoList.Pop()` to return (value, ok)
    This makes it less racy when viewed from the outside, and we can get
    rid of `ToDoList.Length()`.

--- src/sizes/todo.go
+++ src/sizes/todo.go
@@ -11,10 +11,6 @@ type ToDoList struct {
 	list []pending
 }
 
-func (t *ToDoList) Length() int {
-	return len(t.list)
-}
-
 func (t *ToDoList) Push(pending pending) {
 	t.list = append(t.list, pending)
 }
@@ -23,22 +19,25 @@ func (t *ToDoList) PushAll(subtasks ToDoList) {
 	t.list = append(t.list, subtasks.list...)
 }
 
-func (t *ToDoList) Pop() pending {
+func (t *ToDoList) Pop() (pending, bool) {
+	if len(t.list) == 0 {
+		return nil, false
+	}
 	ret := t.list[len(t.list)-1]
 	t.list[len(t.list)-1] = nil
 	t.list = t.list[0 : len(t.list)-1]
-	return ret
+	return ret, true
 }
 
 func (t *ToDoList) Run(scanner *SizeScanner) error {
-	for t.Length() != 0 {
-		p := t.Pop()
-
+	for {
+		p, ok := t.Pop()
+		if !ok {
+			return nil
+		}
 		err := p.Run(scanner)
 		if err != nil {
 			return err
 		}
 	}
-
-	return nil
 }

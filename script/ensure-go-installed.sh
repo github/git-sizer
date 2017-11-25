b7dac53e0d772bc76bb78d9e296b0111a5c48e64
Michael Haggerty <mhagger@github.com>
2017-11-25 08:24:35 +0100

    ensure-go-installed.sh: make version number check more robust
    ...and fix a comment.

--- script/ensure-go-installed.sh
+++ script/ensure-go-installed.sh
@@ -4,14 +4,12 @@ if [ -z "$ROOTDIR" ]; then
     echo 1>&2 'ensure-go-installed.sh invoked without ROOTDIR set!'
 fi
 
-# Is go installed, and at least 1.7?
+# Is go installed, and at least 1.9?
 go_ok() {
-    case "$(go version 2>/dev/null | sed -n 's/.*go\([0-9][0-9]*\.[0-9][0-9]*\).*/\1/p' | head -n 1)" in
-        ""|0.*|1.[012345678])
-            false ;;
-        *)
-            true ;;
-    esac
+    set -- $(go version 2>/dev/null |
+                sed -n 's/.*go\([0-9][0-9]*\)\.\([0-9][0-9]*\).*/\1 \2/p' |
+                head -n 1)
+    [ $# -eq 2 ] && [ "$1" -eq 1 ] && [ "$2" -ge 9 ]
 }
 
 # If a local go is installed, use it.

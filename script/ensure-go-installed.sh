c1d1946924b97dda18bae8b937bd7e61ec6db3a7
Michael Haggerty <mhagger@github.com>
2021-04-19 12:20:07 +0200

    Update to go1.16.3

--- script/ensure-go-installed.sh
+++ script/ensure-go-installed.sh
@@ -4,17 +4,17 @@ if [ -z "$ROOTDIR" ]; then
     echo 1>&2 'ensure-go-installed.sh invoked without ROOTDIR set!'
 fi
 
-# Is go installed, and at least 1.13?
+# Is go installed, and at least 1.16?
 go_ok() {
     set -- $(go version 2>/dev/null |
                 sed -n 's/.*go\([0-9][0-9]*\)\.\([0-9][0-9]*\).*/\1 \2/p' |
                 head -n 1)
-    [ $# -eq 2 ] && [ "$1" -eq 1 ] && [ "$2" -ge 13 ]
+    [ $# -eq 2 ] && [ "$1" -eq 1 ] && [ "$2" -ge 16 ]
 }
 
 # If a local go is installed, use it.
 set_up_vendored_go() {
-    GO_VERSION=go1.13.4
+    GO_VERSION=go1.16.3
     VENDORED_GOROOT="$ROOTDIR/vendor/$GO_VERSION/go"
     if [ -x "$VENDORED_GOROOT/bin/go" ]; then
         export GOROOT="$VENDORED_GOROOT"

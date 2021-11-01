f3caecba4cf28cbc634f03da46771092654b81da
Michael Haggerty <mhagger@github.com>
2021-11-01 17:05:50 +0100

    InOrderPathResolver.forgetPathLocked(): handle unusual case first

--- sizes/path_resolver.go
+++ sizes/path_resolver.go
@@ -256,10 +256,13 @@ func (pr *InOrderPathResolver) forgetPathLocked(p *Path) {
 		panic("forgetPathLocked() called when refcount zero")
 	}
 	p.seekerCount--
+
 	if p.seekerCount > 0 {
 		// The path is still wanted (by another seeker).
 		return
-	} else if p.parent != nil {
+	}
+
+	if p.parent != nil {
 		// We already found the object's parent, and the parent's path
 		// is wanted on account if this object. Decrement its
 		// seekerCount.
